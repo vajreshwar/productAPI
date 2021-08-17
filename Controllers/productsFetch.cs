411ee21159b91b60345c950fe5340a4afea46368
vajreshwar <vajreshwar06@gmail.com>
2021-08-17 15:34:21 +0530

    Did add prods code

--- Controllers/productsFetch.cs
+++ Controllers/productsFetch.cs
@@ -20,11 +20,26 @@ namespace productAPI.Controllers
         }
 
         [HttpGet]
+        [Route("offers")]
         public List<Offer> Get()
         {
            return this.offerService.GetTodaysOffers();
         }
 
+
+        [HttpGet]
+        [Route("products")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(this.offerService.GetAllProducts().OrderBy(x => x.Price).Take(3));
+        }
+
+        [HttpGet]
+        [Route("product")]
+        public IActionResult GetProductWithSecondLowestPrice()
+        {
+            return Ok(this.offerService.GetAllProducts().OrderBy(x => x.Price).Skip(1).Take(1));
+        }
     }
 }
 
